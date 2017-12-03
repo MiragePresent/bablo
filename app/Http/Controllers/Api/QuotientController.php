d4a98a3b1090f99eae524970d54e68aabe671e46
David Holovii <mirage.present@gmail.com>
2017-12-03 14:15:09 +0200

    Create check request

--- app/Http/Controllers/Api/QuotientController.php
+++ app/Http/Controllers/Api/QuotientController.php
@@ -2,10 +2,28 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Requests\CreateCheckRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class QuotientController extends Controller
 {
-    //
+    public function create(CreateCheckRequest $request)
+    {
+
+        /** @var \App\Models\Check $check */
+        $check = \Check::create(array_merge(
+            $request->all(),
+            [
+                'user_id' => auth()->id(),
+                'status'  => \Check::NOT_PAID
+            ]
+        ));
+
+        return response(
+            '',
+            201,
+            [ 'location' => route('api.check.info', [ 'check' => $check->id ]) ]
+        );
+    }
 }
