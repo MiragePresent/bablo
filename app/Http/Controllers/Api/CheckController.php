4971fca11cfe9b82247b979563602a952b783203
David Holovii <mirage.present@gmail.com>
2017-12-03 14:23:09 +0200

    Create Check request

--- app/Http/Controllers/Api/CheckController.php
+++ app/Http/Controllers/Api/CheckController.php
@@ -4,8 +4,26 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateCheckRequest;
 
 class CheckController extends Controller
 {
-    //
+    public function create(CreateCheckRequest $request)
+    {
+
+        /** @var \App\Models\Check $check */
+        $check = \Check::create(array_merge(
+            $request->all(),
+            [
+                'user_id' => auth()->id(),
+                'status'  => \Check::NOT_PAID
+            ]
+        ));
+
+        return response(
+            '',
+            201,
+            [ 'location' => route('api.check.info', [ 'check' => $check->id ]) ]
+        );
+    }
 }
--- app/Http/Controllers/Api/QuotientController.php
+++ app/Http/Controllers/Api/QuotientController.php
@@ -2,28 +2,10 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Requests\CreateCheckRequest;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class QuotientController extends Controller
 {
-    public function create(CreateCheckRequest $request)
-    {
 
-        /** @var \App\Models\Check $check */
-        $check = \Check::create(array_merge(
-            $request->all(),
-            [
-                'user_id' => auth()->id(),
-                'status'  => \Check::NOT_PAID
-            ]
-        ));
-
-        return response(
-            '',
-            201,
-            [ 'location' => route('api.check.info', [ 'check' => $check->id ]) ]
-        );
-    }
 }
