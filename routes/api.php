dae2712ca705cca094c57d9808f82c5b25b78571
David Holovii <mirage.present@gmail.com>
2017-12-06 20:03:37 +0200

    Updated routes

--- routes/api.php
+++ routes/api.php
@@ -17,22 +17,21 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['middleware' => 'auth'], function () {
+Route::get('users/{user}/checks', 'UserController@checks')->name('user.checks');
 
-    Route::post('checks', 'CheckController@create')->name('checks.create');
-    Route::get('checks/{check}', 'CheckController@info')->name('checks.info');
-    Route::patch('checks/{check}', 'CheckController@update')->name('checks.update');
-    Route::delete('checks/{check}', 'CheckController@delete')->name('checks.delete');
+Route::post('checks', 'CheckController@create')->name('checks.create');
+Route::get('checks/{check}', 'CheckController@info')->name('checks.info');
+Route::patch('checks/{check}', 'CheckController@update')->name('checks.update');
+Route::delete('checks/{check}', 'CheckController@delete')->name('checks.delete');
 
-    Route::post('quotients', 'QuotientController@create')->name('quotients.create');
-    Route::get('quotients/{quotient}', 'QuotientController@info')->name('quotients.info');
-    Route::patch('quotients/{quotient}', 'QuotientController@update')->name('quotients.update');
-    Route::delete('quotients/{quotient}', 'QuotientController@delete')->name('quotients.delete');
+Route::post('quotients', 'QuotientController@create')->name('quotients.create');
+Route::get('quotients/{quotient}', 'QuotientController@info')->name('quotients.info');
+Route::patch('quotients/{quotient}', 'QuotientController@update')->name('quotients.update');
+Route::delete('quotients/{quotient}', 'QuotientController@delete')->name('quotients.delete');
 
 
-    Route::post('payments', 'PaymentController@create')->name('payments.create');
-    Route::get('payments/{payment}', 'PaymentController@info')->name('payments.info');
-    Route::patch('payments/{payment}', 'PaymentController@update')->name('payments.update');
-    Route::delete('payments/{payment}', 'PaymentController@approve')->name('payments.approve');
-    Route::delete('payments/{payment}', 'PaymentController@delete')->name('payments.delete');
-});
+Route::post('payments', 'PaymentController@create')->name('payments.create');
+Route::get('payments/{payment}', 'PaymentController@info')->name('payments.info');
+Route::patch('payments/{payment}', 'PaymentController@update')->name('payments.update');
+Route::delete('payments/{payment}', 'PaymentController@approve')->name('payments.approve');
+Route::delete('payments/{payment}', 'PaymentController@delete')->name('payments.delete');
