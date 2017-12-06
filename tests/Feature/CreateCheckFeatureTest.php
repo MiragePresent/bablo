b2b3668493633e75085dc6ac51e1944d798049ac
David Holovii <mirage.present@gmail.com>
2017-12-06 20:05:04 +0200

    Validation requests

---
+++ tests/Feature/CreateCheckFeatureTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateCheckFeatureTest extends TestCase
+{
+    /** @test */
+    public function create_a_check_with_one_quotient_test()
+    {
+
+    }
+}
