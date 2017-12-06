eae02b147ff3d2639b634a37d96ccf67ded08dc9
David Holovii <mirage.present@gmail.com>
2017-12-06 20:34:13 +0200

    phpDoc for User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,18 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
+/**
+ * User Model
+ *
+ * @property int $id
+ * @property string $login
+ * @property string $email
+ * @property string $first_name
+ * @property string $last_name
+ * @property float  $balance
+ * 
+ */
+
 class User extends Authenticatable
 {
     use Notifiable;
