4c8ec14448cef689395887b96207785c812a7cd4
David Holovii <mirage.present@gmail.com>
2017-12-14 18:15:22 +0200

    App page

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/app', function () {
+    return view('app');
+});
 
 Route::get('passport', function () {
         return view('passport');
